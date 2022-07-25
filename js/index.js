c092e492debb6a379af05380580c78eb922b70dd
tayloroenick <99303829+tayloroenick@users.noreply.github.com>
2022-07-25 16:45:43 -0500

    PeerReview
    State of completion before feedback

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
-$(".heroImg button").hover(function() {
-    $(this).css("background-color","red");
-    $(this).css("background-color", "grey");
-});
+$(".resumeButton").hover(
+    function() {
+        $(this).css("background-color","#404A5F");
+    },
+    function(){
+        $(this).css("background-color", "#728196");
+    }
+);
+
+$(".workButton").hover(
+    function() {
+        $(this).css("background-color","#404A5F");
+    },
+    function(){
+        $(this).css("background-color", "#728196");
+    }
+);
+
+
+$(".portfolio").hover(
+    function() {
+        $(this).css("background-color","#404A5F");
+    },
+    function(){
+        $(this).css("background-color", "#728196");
+    }
+);
